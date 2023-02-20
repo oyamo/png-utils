c441270b4e48ef69c32c7bf63ca2b92f85e771d8
oyamo <oyamo.xyz@gmail.com>
2023-02-20 19:10:13 +0300

    add image grayscale

--- grayscale.go
+++ grayscale.go
@@ -1,12 +1,26 @@
 package imageutils
 
 import (
+	"errors"
 	"image"
+	"image/color"
 	"reflect"
 )
 
 func GrayScaleImage(img image.Image) (image.Image, error) {
 	if img == nil || reflect.ValueOf(img).Kind() == reflect.Ptr && reflect.ValueOf(img).IsNil() {
-
+		return nil, errors.New("img is empty")
 	}
+
+	newImgRGBA := image.NewRGBA(img.Bounds())
+	for i := 0; i < newImgRGBA.Bounds().Dx(); i++ {
+		for j := 0; j < newImgRGBA.Bounds().Dy(); j++ {
+			pixel := img.At(i, j)
+			_, _, _, alpha := pixel.RGBA()
+			newPixel := color.RGBA{A: uint8(alpha)}
+			newImgRGBA.SetRGBA(i, j, newPixel)
+		}
+	}
+
+	return newImgRGBA, nil
 }
